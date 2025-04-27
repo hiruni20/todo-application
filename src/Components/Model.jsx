607ecbe7c7a058c2107d9a5fe9b3e3b8eb0e72a5
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-04-27 17:00:46 +0530

    moodel

---
+++ src/Components/Model.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useRef } from 'react'
+
+function Model({children, showMode, setShowModel}) {
+    const modelRef = useRef()
+
+    const closeModel = (e) => {
+        if (e.target === modelRef.current) {
+            setShowModel(false)
+        }
+    }
+  return (
+    showModel &&
+    <div className='Model' onClick={closeModel}>
+        <div className="container">
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Model
