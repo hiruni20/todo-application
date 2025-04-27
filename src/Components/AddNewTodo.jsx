658903cf137b48a0532b65e885f7df4874d4ca40
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-04-28 00:23:57 +0530

    model

--- src/Components/AddNewTodo.jsx
+++ src/Components/AddNewTodo.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Model from './Model'
 
 function AddNewTodo() {
+  const [showModel, setShowModel] = useState(false)
   return (
-    <div className='addnewtodo'>AddNewTodo</div>
+    <div className='addnewtodo'>
+      <button>+ New Todo</button>
+      <Model showModel={showModel} setShowModel={setShowModel} >
+        <div>jjjj</div>
+      </Model>
+    </div>
   )
 }
 
